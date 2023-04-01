193f8886bb28b60169f49dc0cfc58f9740a0868e
satyadev540 <76416111+satyadev540@users.noreply.github.com>
2023-04-01 20:18:00 +0530

    BloodBank

---
+++ index.js
@@ -0,0 +1,59 @@
+//document.getElementById("lr").onclick = function() {lrl()}
+function lrl()
+{
+    document.getElementById("btext").style.display="none"
+    document.getElementById("lh").style.display="none"
+    document.getElementById("lr").style.display="none"
+    document.getElementById("login").style.display="flex"
+    document.getElementById("login").style.justifyContent = "center"
+}
+function lhl()
+{
+    document.getElementById("btext").style.display="none"
+    document.getElementById("lh").style.display="none"
+    document.getElementById("lr").style.display="none"
+    document.getElementById("login").style.display="flex"
+    document.getElementById("login").style.justifyContent = "center"
+}
+function cancel()
+{
+    document.getElementById("btext").style.display="block"
+    document.getElementById("lh").style.display="inline-block"
+    document.getElementById("lr").style.display="inline-block"
+    document.getElementById("login").style.display="none"
+    
+}
+function rrr()
+{
+    document.getElementById("btext").style.display="none"
+    document.getElementById("rr").style.display="none"
+    document.getElementById("rh").style.display="none"
+    document.getElementById("registration2").style.display="flex"
+    document.getElementById("registration2").style.justifyContent = "center"
+    
+
+}
+function rhr()
+{
+    document.getElementById("btext").style.display="none"
+    document.getElementById("rr").style.display="none"
+    document.getElementById("rh").style.display="none"
+    document.getElementById("registration").style.display="flex"
+    document.getElementById("registration").style.justifyContent = "center"
+}
+function cancel1()
+{
+    document.getElementById("btext").style.display="block"
+    document.getElementById("rr").style.display="inline-block"
+    document.getElementById("registration").style.display="none"
+    document.getElementById("rh").style.display="inline-block"
+}
+function cancel2()
+{
+    document.getElementById("btext").style.display="block"
+    document.getElementById("rr").style.display="inline-block"
+    document.getElementById("registration2").style.display="none"
+    document.getElementById("rh").style.display="inline-block"
+}
+
+
